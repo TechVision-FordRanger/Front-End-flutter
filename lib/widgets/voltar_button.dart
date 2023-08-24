ec1dc4ec19bbe318bf06ae14ebe775001ec9418f
Jorge Oliveira <jorge.oliveira@urbanobank.com>
2023-08-24 20:46:24 -0300

    tela de cadastro V1

---
+++ lib/widgets/voltar_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class VoltarButton extends StatelessWidget {
+  final Function onPressed;
+
+  VoltarButton({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onPressed(),
+      child: Container(
+        width: 60.0,  // Ajuste conforme necessário
+        height: 60.0, // Ajuste conforme necessário
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/back_button.png'),
+            fit: BoxFit.contain, // Isso garantirá que a imagem se ajuste ao tamanho do container.
+          ),
+        ),
+      ),
+    );
+  }
+}
